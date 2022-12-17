2df827d7352ababa710e18fd3f7231337efbe704
angel <perdonholaperdon123@gmail.com>
2022-12-17 17:46:11 -0400

    adding_valiadations_path_parameters

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from typing import Optional
 from pydantic import BaseModel
 
 #Fastapi
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI
+from fastapi import Body, Query, Path
 
 
 class Person(BaseModel):
@@ -30,10 +31,31 @@ def create_perseon(person: Person = Body(...)):
 #Validaciones: query parameters
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: str = Query(...)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title = "Person name",
+        description= "This is the person name. The number of characters must be between 1 and 50"
+        ),
+    age: str = Query(
+        ...,
+        title = "Person age",
+        description = "This is the person age. It's required"
+        )
 ):
     return{name: age}
 
+#Validaciones: path parameters
 
+@app.get("/person/datail/{person_id}")
+def show_person_id(
+    person_id: int = Path(
+        ..., 
+        ge=1,
+        title = "Person id",
+        description = "This is the identifier of the person. These parameter must be greater than 0"
+        )
+):
+    return{person_id: "It is person"}
 
